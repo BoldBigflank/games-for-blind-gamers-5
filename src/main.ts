9951f19f1f9f5f02345dbbfac361f8e99d6b4f75
Alex Swan <alex@bold-it.com>
2026-02-17 18:51:33 -0600

    framework

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ import { setupWebSocketGameLobbyClient } from './SocketClient.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Games for Blind Gamers 5</h1>
-    <div id="rooms-list"></div>
-    <button id="create-room" type="button">Create Room</button>
+    <div id="lobby">
+      <div id="rooms-list">
+      <button id="create-room" type="button">Create Room</button>
+    </div>
+    </div>
+    <div id="game-info">Game Info</div>
+    <div id="game-actions">
+      Game Actions
+      <div id="game-actions-prompt"></div>
+      <div id="game-actions-buttons">
+      </div>
+    </div>
     <div>
       <textarea id="channel-messages" rows="20" cols="100"></textarea>
     </div>
